c985e5919b8e45071b91c42eccc98d1ccecd1e25
fcriscuo <fcriscu1@jhu.edu>
2013-12-10 12:53:20 -0800

    changes to IFunctionTerm

--- src/main/java/org/ndexbio/rest/domain/IFunctionTerm.java
+++ src/main/java/org/ndexbio/rest/domain/IFunctionTerm.java
@@ -3,16 +3,15 @@ package org.ndexbio.rest.domain;
 import com.tinkerpop.frames.Adjacency;
 import com.tinkerpop.frames.Property;
 import com.tinkerpop.frames.modules.typedgraph.TypeValue;
-import java.util.Map;
 
 @TypeValue("Function")
 public interface IFunctionTerm extends ITerm
 {
     @Property("termParameters")
-    public Map<Integer, ITerm> getTermParameters();
+    public Iterable<ITerm> getTermParameters();
 
     @Property("termParameters")
-    public void setTermParameters(Map<Integer, ITerm> termParameters);
+    public void setTermParameters(Iterable< ITerm> termParameters);
 
     @Adjacency(label = "termFunc")
     public IBaseTerm getTermFunc();
@@ -20,9 +19,5 @@ public interface IFunctionTerm extends ITerm
     @Adjacency(label = "termFunc")
     public void setTermFunc(IBaseTerm term);
 
-    @Property("textParameters")
-    public Map<Integer, String> getTextParameters();
-
-    @Property("textParameters")
-    public void setTextParameters(Map<Integer, String> textParameters);
+    
 }
