f23ebe8ef92a17e266dd99594cbb305492065b0f
fcriscuo <fcriscu1@jhu.edu>
2014-02-04 17:33:48 -0800

    Interface to support commnon literal values in project

---
+++ src/main/java/org/ndexbio/rest/CommonValues.java
@@ -0,0 +1,12 @@
+package org.ndexbio.rest;
+
+/*
+ * Represents a set of literals used throughout the ndexbio-rest project
+ */
+
+public interface CommonValues {
+
+	public static final String SEARCH_MATCH_EXACT = "exact-match";
+	public static final String SEARCH_MATCH_CONTAINS = "contains";
+	public static final String SEARCH_MATCH_STARTS_WITH = "starts-with";
+}
