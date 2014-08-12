98480eabdc62c3bdc0f6db3acfb971594d1f5a45
Ricardo Rivas <ricardo.rivas.preuss12@gmail.com>
2014-08-12 12:17:53 -0700

    Temp fix to CORS

--- src/main/java/org/ndexbio/rest/filters/CrossOriginResourceSharingFilter.java
+++ src/main/java/org/ndexbio/rest/filters/CrossOriginResourceSharingFilter.java
@@ -42,11 +42,22 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.MultivaluedMap;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
 import javax.ws.rs.ext.Provider;
 
 @Provider
-public class CrossOriginResourceSharingFilter implements ContainerResponseFilter
+public class CrossOriginResourceSharingFilter implements ContainerResponseFilter, Filter
 {
+	//ContainerResponseFilter Implementation
 	@Override
 	public void filter(ContainerRequestContext arg0,
 			ContainerResponseContext responseContext) throws IOException {
@@ -57,5 +68,28 @@ public class CrossOriginResourceSharingFilter implements ContainerResponseFilter
 		headers.putSingle("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
 		headers.putSingle("Access_Control_Allow_Credentials", true);
 	}
+	
+	//Filter Implementation
+	@Override
+    public void destroy()
+    {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
+    {
+        HttpServletResponse httpResponse = (HttpServletResponse)response;
+        httpResponse.addHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.addHeader("Access-Control-Allow-Methods", "DELETE,GET,OPTIONS,POST,PUT");
+        httpResponse.addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+        
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig config) throws ServletException
+    {
+    }
+	
 }
 
