794d6f18ac74e6f1ac9bc5e1285d0134d613c383
Dexter Pratt <dexterpratt.bio@gmail.com>
2014-08-21 15:17:12 -0700

    Add getTasks function, cleaned out some obsolete code for images

--- src/main/java/org/ndexbio/rest/services/UserService.java
+++ src/main/java/org/ndexbio/rest/services/UserService.java
@@ -17,6 +17,8 @@ import javax.ws.rs.core.Response;
 import org.ndexbio.model.object.Membership;
 import org.ndexbio.model.object.Permissions;
 import org.ndexbio.model.object.Request;
+import org.ndexbio.model.object.Status;
+import org.ndexbio.model.object.Task;
 import org.ndexbio.model.object.User;
 import org.ndexbio.model.object.NewUser;
 import org.ndexbio.common.models.dao.orientdb.UserDAO;
@@ -202,80 +204,7 @@ public class UserService extends NdexService {
 		}
 	}
 
-	/**************************************************************************
-	 * Adds a network to the user's Work Surface.
-	 * 
-	 * @param userId
-	 *            The user's ID.
-	 * @param networkToAdd
-	 *            The network to add.
-	 * @throws IllegalArgumentException
-	 *             Bad input.
-	 * @throws ObjectNotFoundException
-	 *             The network wasn't found.
-	 * @throws NdexException
-	 *             Failed to add the network in the database.
-	 **************************************************************************/
-/*	@PUT
-	@Path("/work-surface")
-	@Produces("application/json")
-	@ApiDoc("Add a network to the authenticated user's worksurface, returns the current list of networks on the worksurface. Errors if the network does not exist or is already on the worksurface.")
-	public Iterable<Network> addNetworkToWorkSurface(String networkId)
-			throws IllegalArgumentException, ObjectNotFoundException,
-			NdexException {
-		Preconditions.checkArgument(!Strings.isNullOrEmpty(networkId),
-				"A network id is required");
 
-		networkId = networkId.replace("\"", "");
-
-		final ORID userRid = IdConverter.toRid(this.getLoggedInUser().getId());
-		final ORID networkRid = IdConverter.toRid(networkId);
-
-		try {
-			setupDatabase();
-
-			final IUser user = _orientDbGraph.getVertex(userRid, IUser.class);
-
-			final INetwork network = _orientDbGraph.getVertex(networkRid,
-					INetwork.class);
-			if (network == null)
-				throw new ObjectNotFoundException("Network", networkId);
-
-			final Iterable<INetwork> workSurface = user.getWorkSurface();
-			if (workSurface != null) {
-				for (INetwork checkNetwork : workSurface) {
-					if (checkNetwork.asVertex().getId().equals(networkRid))
-						throw new DuplicateObjectException("Network with RID: "
-								+ networkRid
-								+ " is already on the Work Surface.");
-				}
-			}
-
-			user.addNetworkToWorkSurface(network);			
-
-			final ArrayList<Network> updatedWorkSurface = Lists.newArrayList();
-			final Iterable<INetwork> onWorkSurface = user.getWorkSurface();
-			if (onWorkSurface != null) {
-				for (INetwork workSurfaceNetwork : onWorkSurface)
-					updatedWorkSurface.add(new Network(workSurfaceNetwork));
-			}
-
-			return updatedWorkSurface;
-		} catch (DuplicateObjectException | ObjectNotFoundException onfe) {
-			throw onfe;
-		} catch (Exception e) {
-			_logger.error(
-					"Failed to add a network to "
-							+ this.getLoggedInUser().getUsername()
-							+ "'s Work Surface.", e);
-		
-			throw new NdexException(
-					"Failed to add the network to your Work Surface.");
-		} finally {
-			teardownDatabase();
-		}
-	}
-*/
 	/**************************************************************************
 	 * Authenticates a user trying to login.
 	 * 
@@ -347,143 +276,7 @@ public class UserService extends NdexService {
 		}
 	}
 
-	/**************************************************************************
-	 * Changes a user's profile/background image.
-	 * 
-	 * @param imageType
-	 *            The image type.
-	 * @param uploadedImage
-	 *            The uploaded image.
-	 * @throws IllegalArgumentException
-	 *             Bad input.
-	 * @throws NdexException
-	 *             Failed to save the image.
-	 **************************************************************************/
-/*	@POST
-	@Path("/image/{imageType}")
-	@Consumes("multipart/form-data")
-	@Produces("application/json")
-	@ApiDoc("Uploads and installs a new image file to be used as either the profile or background image (depending on the imageType route parameter) for the authenticated user.")
-	public void changeProfileImage(
-			@PathParam("imageType") final String imageType,
-			@MultipartForm UploadedFile uploadedImage)
-			throws IllegalArgumentException, NdexException {
-		if (imageType == null || imageType.isEmpty())
-			throw new IllegalArgumentException("No image type specified.");
-		else if (uploadedImage == null
-				|| uploadedImage.getFileData().length < 1)
-			throw new IllegalArgumentException("No uploaded image.");
 
-		final ORID userId = IdConverter.toRid(this.getLoggedInUser().getId());
-
-		try {
-			setupDatabase();
-
-			final IUser user = _orientDbGraph.getVertex(userId, IUser.class);
-			if (user == null)
-				throw new ObjectNotFoundException("User", this
-						.getLoggedInUser().getId());
-
-			final BufferedImage newImage = ImageIO
-					.read(new ByteArrayInputStream(uploadedImage.getFileData()));
-
-			if (imageType.toLowerCase().equals("profile")) {
-				final BufferedImage resizedImage = resizeImage(newImage,
-						Integer.parseInt(Configuration.getInstance()
-								.getProperty("Profile-Image-Width")),
-						Integer.parseInt(Configuration.getInstance()
-								.getProperty("Profile-Image-Height")));
-
-				ImageIO.write(
-						resizedImage,
-						"jpg",
-						new File(Configuration.getInstance().getProperty(
-								"Profile-Image-Path")
-								+ user.getUsername() + ".jpg"));
-			} else {
-				final BufferedImage resizedImage = resizeImage(newImage,
-						Integer.parseInt(Configuration.getInstance()
-								.getProperty("Profile-Background-Width")),
-						Integer.parseInt(Configuration.getInstance()
-								.getProperty("Profile-Background-Height")));
-
-				ImageIO.write(resizedImage, "jpg", new File(Configuration
-						.getInstance().getProperty("Profile-Background-Path")
-						+ user.getUsername() + ".jpg"));
-			}
-		} catch (ObjectNotFoundException onfe) {
-			throw onfe;
-		} catch (Exception e) {
-			_logger.error("Failed to save a profile image.", e);
-			throw new NdexException(e.getMessage());
-		} finally {
-			teardownDatabase();
-		}
-	}
-*/
-	
-	/**************************************************************************
-	 * Deletes a network from a user's Work Surface.
-	 * 
-	 * @param networkToDelete
-	 *            The network being removed.
-	 * @throws IllegalArgumentException
-	 *             Bad input.
-	 * @throws ObjectNotFoundException
-	 *             The network doesn't exist.
-	 * @throws NdexException
-	 *             Failed to remove the network in the database.
-	 **************************************************************************/
-/*	@DELETE
-	@Path("/work-surface/{networkId}")
-	@Produces("application/json")
-	@ApiDoc("Removes the network with id = networkId from the worksurface of the authenticated user, returning the list of networks on the worksurface. Errors if the network is not found.")
-	public Iterable<Network> deleteNetworkFromWorkSurface(
-			@PathParam("networkId") final String networkId)
-			throws IllegalArgumentException, ObjectNotFoundException,
-			NdexException {
-		Preconditions.checkArgument(!Strings.isNullOrEmpty(networkId), 
-				"A network id is required");
-	
-		final ORID userRid = IdConverter.toRid(this.getLoggedInUser().getId());
-		final ORID networkRid = IdConverter.toRid(networkId);
-
-		try {
-			setupDatabase();
-
-			final IUser user = _orientDbGraph.getVertex(userRid, IUser.class);
-
-			final INetwork network = _orientDbGraph.getVertex(networkRid,
-					INetwork.class);
-			if (network == null)
-				throw new ObjectNotFoundException("Network", networkId);
-
-			user.removeNetworkFromWorkSurface(network);
-			
-
-			final ArrayList<Network> updatedWorkSurface = Lists.newArrayList();
-			final Iterable<INetwork> onWorkSurface = user.getWorkSurface();
-			if (onWorkSurface != null) {
-				for (INetwork workSurfaceNetwork : onWorkSurface)
-					updatedWorkSurface.add(new Network(workSurfaceNetwork));
-			}
-
-			return updatedWorkSurface;
-		} catch (ObjectNotFoundException onfe) {
-			throw onfe;
-		} catch (Exception e) {
-			_logger.error(
-					"Failed to remove a network from "
-							+ this.getLoggedInUser().getUsername()
-							+ "'s Work Surface.", e);
-			
-			throw new NdexException(
-					"Failed to remove the network from your Work Surface.");
-		} finally {
-			teardownDatabase();
-		}
-	}
-*/
 	/**************************************************************************
 	 * Deletes a user.
 	 * 
@@ -584,53 +377,7 @@ public class UserService extends NdexService {
 		
 	}
 
-	/**************************************************************************
-	 * Gets a user by ID or username.
-	 * 
-	 * @param userId
-	 *            The ID or username of the user.
-	 * @throws IllegalArgumentException
-	 *             Bad input.
-	 * @throws NdexException
-	 *             Failed to change the password in the database.
-	 **************************************************************************/
-/*	@GET
-	@PermitAll
-	@Path("/{userId}")
-	@Produces("application/json")
-	@ApiDoc("Return the user corresponding to userId, whether userId is actually a database id or a username. Error if neither is found.")
-	public User getUser(@PathParam("userId") final String userId)
-			throws IllegalArgumentException, NdexException {
-		if (userId == null || userId.isEmpty())
-			throw new IllegalArgumentException("No user ID was specified.");
 
-		try {
-			setupDatabase();
-
-			final ORID userRid = IdConverter.toRid(userId);
-
-			final IUser user = _orientDbGraph.getVertex(userRid, IUser.class);
-			if (user != null)
-				return new User(user, true);
-		} catch (IllegalArgumentException ae) {
-			// The user ID is actually a username
-			final List<ODocument> matchingUsers = _ndexDatabase
-					.query(new OSQLSynchQuery<Object>(
-							"SELECT FROM User WHERE username = '" + userId
-									+ "'"));
-			if (!matchingUsers.isEmpty())
-				return new User(_orientDbGraph.getVertex(matchingUsers.get(0),
-						IUser.class), true);
-		} catch (Exception e) {
-			_logger.error("Failed to get user: " + userId + ".", e);
-			throw new NdexException("Failed to retrieve that user.");
-		} finally {
-			teardownDatabase();
-		}
-
-		return null;
-	}
-*/
 	/**************************************************************************
 	 * Updates a user.
 	 * 
@@ -715,7 +462,7 @@ public class UserService extends NdexService {
 	@Path("/{userId}/request/pending/{skipBlocks}/{blockSize}")
 	@Produces("application/json")
 	@ApiDoc("")
-	public List<Request> getPendingRequest(@PathParam("userId") final String userId,
+	public List<Request> getPendingRequests(@PathParam("userId") final String userId,
 			@PathParam("skipBlocks") int skipBlocks,
 			@PathParam("blockSize") int blockSize) throws NdexException {
 		
@@ -724,6 +471,27 @@ public class UserService extends NdexService {
 		try {
 			
 			return dao.getPendingRequest(this.getLoggedInUser(),skipBlocks, blockSize);
+
+		} finally {
+			this.closeDatabase();
+		}
+	}
+	
+	@GET
+	@Path("/{userId}/task/{status}/{skipBlocks}/{blockSize}")
+	@Produces("application/json")
+	@ApiDoc("Returns an array of Task objects with the specified status")
+	public List<Task> getTasks(
+			@PathParam("userId") final String userId,
+			@PathParam("status") final String status,
+			@PathParam("skipBlocks") int skipBlocks,
+			@PathParam("blockSize") int blockSize) throws NdexException {
+		
+		this.openDatabase();
+		
+		try {
+			Status taskStatus = Status.valueOf(status);
+			return dao.getTasks(this.getLoggedInUser(),taskStatus, skipBlocks, blockSize);
 
 		} finally {
 			this.closeDatabase();
@@ -745,25 +513,4 @@ public class UserService extends NdexService {
 	
 	
 
-	/**************************************************************************
-	 * Resizes the source image to the specified dimensions.
-	 * 
-	 * @param sourceImage
-	 *            The image to resize.
-	 * @param width
-	 *            The new image width.
-	 * @param height
-	 *            The new image height.
-	 **************************************************************************/
-/*	private BufferedImage resizeImage(final BufferedImage sourceImage,
-			final int width, final int height) {
-		final Image resizeImage = sourceImage.getScaledInstance(width, height,
-				Image.SCALE_SMOOTH);
-
-		final BufferedImage resizedImage = new BufferedImage(width, height,
-				Image.SCALE_SMOOTH);
-		resizedImage.getGraphics().drawImage(resizeImage, 0, 0, null);
-
-		return resizedImage;
-	} */
 }
