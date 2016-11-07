b0660ceaff56a1a914f2f2ddf65dc20c0f4f6e8f
chenjing <jingjingbic@gmail.com>
2016-11-07 15:49:10 -0800

    Updated postgesql schema.

--- src/main/resources/postgresql/ndex_db_schema.sql
+++ src/main/resources/postgresql/ndex_db_schema.sql
@@ -111,7 +111,8 @@ CREATE TABLE ndex_group (
     is_deleted boolean,
     other_attributes jsonb,
     modification_time timestamp without time zone,
-    website_url character varying(200)
+    website_url character varying(200),
+    is_from_13 boolean
 );
 
 
@@ -171,7 +172,8 @@ CREATE TABLE ndex_user (
     description text,
     is_deleted boolean,
     other_attributes jsonb,
-    is_verified boolean
+    is_verified boolean,
+    is_from_13 boolean
 );
 
 
@@ -213,7 +215,8 @@ CREATE TABLE network (
     is_validated boolean DEFAULT false,
     readonly boolean,
     error character varying(2000),
-    warnings text[]
+    warnings text[],
+    is_from_13 boolean
 );
 
 
@@ -252,6 +255,13 @@ COMMENT ON COLUMN network.error IS 'store the last error message if server faile
 --
 
 COMMENT ON COLUMN network.warnings IS 'Stores warnings.';
+
+
+--
+-- Name: COLUMN network.is_from_13; Type: COMMENT; Schema: core; Owner: ndexserver
+--
+
+COMMENT ON COLUMN network.is_from_13 IS 'true means this network is migrated from Ndex 1.3';
 
 
 --
@@ -371,6 +381,170 @@ CREATE TABLE user_network_membership_arc (
 ALTER TABLE user_network_membership_arc OWNER TO ndexserver;
 
 --
+-- Name: v1_group; Type: TABLE; Schema: core; Owner: ndexserver
+--
+
+CREATE TABLE v1_group (
+    rid character varying(20),
+    account_name character varying(300),
+    group_name character varying(100),
+    website_url character varying(300),
+    image_url character varying(200),
+    modification_time timestamp without time zone,
+    creation_time timestamp without time zone,
+    description text,
+    id uuid NOT NULL
+);
+
+
+ALTER TABLE v1_group OWNER TO ndexserver;
+
+--
+-- Name: v1_group_network; Type: TABLE; Schema: core; Owner: ndexserver
+--
+
+CREATE TABLE v1_group_network (
+    group_id uuid NOT NULL,
+    network_rid character varying(10) NOT NULL,
+    type character varying NOT NULL
+);
+
+
+ALTER TABLE v1_group_network OWNER TO ndexserver;
+
+--
+-- Name: v1_network; Type: TABLE; Schema: core; Owner: ndexserver
+--
+
+CREATE TABLE v1_network (
+    rid character varying(20),
+    id uuid NOT NULL,
+    creation_time timestamp without time zone,
+    modification_time timestamp without time zone,
+    visibility character varying(10),
+    node_count bigint,
+    edge_count bigint,
+    description text,
+    version text,
+    props json,
+    provenance json,
+    readonly boolean,
+    name text,
+    source_format character varying(100),
+    owner character varying(50)
+);
+
+
+ALTER TABLE v1_network OWNER TO ndexserver;
+
+--
+-- Name: v1_request; Type: TABLE; Schema: core; Owner: ndexserver
+--
+
+CREATE TABLE v1_request (
+    rid character varying(20),
+    id uuid NOT NULL,
+    source_uuid uuid,
+    destination_uuid uuid,
+    message text,
+    creation_time timestamp without time zone,
+    modification_time timestamp without time zone,
+    responder character varying(20),
+    response_message text,
+    response character varying(20),
+    in_request character varying(100),
+    out_request character varying(100),
+    request_permission character varying(50),
+    response_time timestamp without time zone
+);
+
+
+ALTER TABLE v1_request OWNER TO ndexserver;
+
+--
+-- Name: v1_task; Type: TABLE; Schema: core; Owner: ndexserver
+--
+
+CREATE TABLE v1_task (
+    id uuid NOT NULL,
+    creation_time timestamp without time zone,
+    modification_time timestamp with time zone,
+    description text,
+    status character varying(20),
+    task_type character varying(30),
+    resource uuid,
+    start_time timestamp without time zone,
+    end_time timestamp without time zone,
+    rid character varying(20),
+    owneruuid uuid,
+    format character varying(30),
+    attributes json
+);
+
+
+ALTER TABLE v1_task OWNER TO ndexserver;
+
+--
+-- Name: v1_user; Type: TABLE; Schema: core; Owner: ndexserver
+--
+
+CREATE TABLE v1_user (
+    rid character varying(50),
+    id uuid NOT NULL,
+    creation_time timestamp without time zone,
+    modification_time timestamp without time zone,
+    account_name character varying(200),
+    password character varying(500),
+    description text,
+    email character varying(100),
+    first_name character varying(100),
+    last_name character varying(100),
+    image_url character varying(200),
+    website_url character varying(200)
+);
+
+
+ALTER TABLE v1_user OWNER TO ndexserver;
+
+--
+-- Name: v1_user_group; Type: TABLE; Schema: core; Owner: ndexserver
+--
+
+CREATE TABLE v1_user_group (
+    user_id uuid NOT NULL,
+    group_rid character varying(10) NOT NULL,
+    type character varying(10) NOT NULL
+);
+
+
+ALTER TABLE v1_user_group OWNER TO ndexserver;
+
+--
+-- Name: v1_user_network; Type: TABLE; Schema: core; Owner: ndexserver
+--
+
+CREATE TABLE v1_user_network (
+    user_id uuid NOT NULL,
+    network_rid character varying(20) NOT NULL,
+    type character varying(10) NOT NULL
+);
+
+
+ALTER TABLE v1_user_network OWNER TO ndexserver;
+
+--
+-- Name: working_migrated_uuids; Type: TABLE; Schema: core; Owner: ndexserver
+--
+
+CREATE TABLE working_migrated_uuids (
+    table_name character varying(50),
+    id uuid NOT NULL
+);
+
+
+ALTER TABLE working_migrated_uuids OWNER TO ndexserver;
+
+--
 -- Name: groupNetworkMembership_pkey; Type: CONSTRAINT; Schema: core; Owner: ndexserver
 --
 
@@ -384,6 +558,14 @@ ALTER TABLE ONLY group_network_membership
 
 ALTER TABLE ONLY ndex_group
     ADD CONSTRAINT group_pk PRIMARY KEY ("UUID");
+
+
+--
+-- Name: migrated_uuids_pkey; Type: CONSTRAINT; Schema: core; Owner: ndexserver
+--
+
+ALTER TABLE ONLY working_migrated_uuids
+    ADD CONSTRAINT migrated_uuids_pkey PRIMARY KEY (id);
 
 
 --
@@ -432,6 +614,70 @@ ALTER TABLE ONLY user_network_membership
 
 ALTER TABLE ONLY ndex_user
     ADD CONSTRAINT user_pk PRIMARY KEY ("UUID");
+
+
+--
+-- Name: v1_group_network_pkey; Type: CONSTRAINT; Schema: core; Owner: ndexserver
+--
+
+ALTER TABLE ONLY v1_group_network
+    ADD CONSTRAINT v1_group_network_pkey PRIMARY KEY (group_id, network_rid, type);
+
+
+--
+-- Name: v1_group_pkey; Type: CONSTRAINT; Schema: core; Owner: ndexserver
+--
+
+ALTER TABLE ONLY v1_group
+    ADD CONSTRAINT v1_group_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: v1_network_pkey; Type: CONSTRAINT; Schema: core; Owner: ndexserver
+--
+
+ALTER TABLE ONLY v1_network
+    ADD CONSTRAINT v1_network_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: v1_request_pkey; Type: CONSTRAINT; Schema: core; Owner: ndexserver
+--
+
+ALTER TABLE ONLY v1_request
+    ADD CONSTRAINT v1_request_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: v1_task_pkey; Type: CONSTRAINT; Schema: core; Owner: ndexserver
+--
+
+ALTER TABLE ONLY v1_task
+    ADD CONSTRAINT v1_task_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: v1_user_group_pkey; Type: CONSTRAINT; Schema: core; Owner: ndexserver
+--
+
+ALTER TABLE ONLY v1_user_group
+    ADD CONSTRAINT v1_user_group_pkey PRIMARY KEY (user_id, group_rid, type);
+
+
+--
+-- Name: v1_user_network_pkey; Type: CONSTRAINT; Schema: core; Owner: ndexserver
+--
+
+ALTER TABLE ONLY v1_user_network
+    ADD CONSTRAINT v1_user_network_pkey PRIMARY KEY (user_id, network_rid, type);
+
+
+--
+-- Name: v1_user_pkey; Type: CONSTRAINT; Schema: core; Owner: ndexserver
+--
+
+ALTER TABLE ONLY v1_user
+    ADD CONSTRAINT v1_user_pkey PRIMARY KEY (id);
 
 
 --
@@ -530,6 +776,27 @@ CREATE UNIQUE INDEX user_emailaddr_constraint ON ndex_user USING btree (email_ad
 --
 
 CREATE UNIQUE INDEX user_username_constraint ON ndex_user USING btree (user_name) WHERE (NOT is_deleted);
+
+
+--
+-- Name: v1_group_rid_idx; Type: INDEX; Schema: core; Owner: ndexserver
+--
+
+CREATE UNIQUE INDEX v1_group_rid_idx ON v1_group USING btree (rid);
+
+
+--
+-- Name: v1_network_rid_idx; Type: INDEX; Schema: core; Owner: ndexserver
+--
+
+CREATE UNIQUE INDEX v1_network_rid_idx ON v1_network USING btree (rid);
+
+
+--
+-- Name: v1_user_network_network_rid_idx; Type: INDEX; Schema: core; Owner: ndexserver
+--
+
+CREATE INDEX v1_user_network_network_rid_idx ON v1_user_network USING btree (network_rid);
 
 
 --
