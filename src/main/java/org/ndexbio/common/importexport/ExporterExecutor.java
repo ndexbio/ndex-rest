0cce0f1872db27e959a6096b5567aa0b22231f3c
Chris Churas <churas.camera@gmail.com>
2018-11-13 18:30:11 -0800

    added javadoc to export method and made a new method createOutputDirectory which handles user directory creation. Also leveraged the IOThreadHandler to write standard error output to a file with .stderr extension to same directory as output

--- src/main/java/org/ndexbio/common/importexport/ExporterExecutor.java
+++ src/main/java/org/ndexbio/common/importexport/ExporterExecutor.java
@@ -7,15 +7,18 @@ import java.io.OutputStream;
 import java.nio.file.StandardCopyOption;
 import java.sql.SQLException;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 import org.apache.commons.io.IOUtils;
-import org.ndexbio.common.models.dao.postgresql.TaskDAO;
 import org.ndexbio.model.exceptions.NdexException;
 import org.ndexbio.model.exceptions.ObjectNotFoundException;
-import org.ndexbio.model.object.Status;
 import org.ndexbio.rest.Configuration;
 
 public class ExporterExecutor {
+	
+    private Logger _logger = Logger.getLogger(ExporterExecutor.class.getSimpleName());
+
+	public static final String STDERR_EXT = ".stderr";
 	
 	private ImporterExporterEntry impExp;
 	
@@ -23,42 +26,58 @@ public class ExporterExecutor {
 		impExp = impExpEntry;
 	}
 
-	public void export(InputStream input, UUID taskId, UUID userId) throws IOException, InterruptedException, SQLException, ObjectNotFoundException, NdexException {
+	/**
+	 * Runs external command line process that is fed CX format network via standard in,
+	 * writing standard output from command line process to a file which is assumed to
+	 * be results of exporter
+	 * 
+	 * @param input CX format network as an InputStream
+	 * @param taskId Id of task
+	 * @param userId Id of user
+	 * @return exit code of command line process which is 0 for success otherwise failure
+	 * @throws IOException
+	 * @throws InterruptedException
+	 * @throws SQLException
+	 * @throws ObjectNotFoundException
+	 * @throws NdexException
+	 */
+	public int export(InputStream input, UUID taskId, UUID userId) throws IOException, InterruptedException, SQLException, ObjectNotFoundException, NdexException {
 		
-		//prepare the diretory
-		String pathPrefix = Configuration.getInstance().getNdexRoot() + "/workspace/" + userId.toString();
-        File archiveDir = new File(pathPrefix);
-        if (!archiveDir.exists())
-        	archiveDir.mkdirs();
-        
-	
+        String pathPrefix = this.createOutputDirectory(userId);
 		ProcessBuilder pb = new ProcessBuilder(impExp.getExporterCmd());
 		pb.directory(new File(impExp.getDirectoryName()));
 		Process p = pb.start();
 		try (OutputStream out = p.getOutputStream()) {
 			try (InputStream in = p.getInputStream() ) {
-				IOThreadHandler expHandler = new IOThreadHandler(in, pathPrefix+ "/"+ taskId + "." + impExp.getFileExtension());
-				expHandler.start();
-		
-				IOUtils.copy(input,out);
-				input.close();
-				out.close();
-				p.waitFor();
+				try (InputStream err = p.getErrorStream()) {
+					IOThreadHandler expHandler = new IOThreadHandler(in, pathPrefix+ "/"+ taskId + "." + impExp.getFileExtension());
+					expHandler.start();
+					IOThreadHandler errHandler = new IOThreadHandler(err, pathPrefix +
+							File.separator + taskId + ExporterExecutor.STDERR_EXT);
+					errHandler.start();
+					IOUtils.copy(input,out);
+					input.close();
+					out.close();
+					p.waitFor();
+				}
 			}			
 		}
-		int exitCode = p.exitValue();
-		
-		// update task
-		try (TaskDAO dao = new TaskDAO()) {
-			if (exitCode == 0 )
-				dao.updateTaskStatus(taskId, Status.COMPLETED);
-			else 
-				dao.updateTaskStatus(taskId, Status.FAILED, "", "");
-			dao.commit();
-		}
-		
+		return p.exitValue();
 	}
 		
+	/**
+	 * Creates directory for user that is used to write export results to
+	 * @param userID Id of user
+	 * @throws IOException if there is a problem creating the directory
+	 */
+	protected String createOutputDirectory(UUID userId) throws IOException {
+		String pathPrefix = Configuration.getInstance().getNdexRoot() + "/workspace/" + userId.toString();
+        File archiveDir = new File(pathPrefix);
+        if (!archiveDir.exists())
+        	_logger.fine("Creating directory: " + archiveDir.getCanonicalPath());
+        	archiveDir.mkdirs();
+        return pathPrefix;
+	}
 	
 	private static class IOThreadHandler extends Thread {
 		
@@ -83,10 +102,7 @@ public class ExporterExecutor {
 				  java.nio.file.Files.copy(
 					      inputStream, 
 					      targetFile.toPath(), 
-					      StandardCopyOption.REPLACE_EXISTING);
-					 
-				//	    IOUtils.closeQuietly(inputStream);
-		
+					      StandardCopyOption.REPLACE_EXISTING);		
 		    } catch (IOException e) {
 		    	errorMessage = e.getMessage();
 		    }
