1603fe82a667ee06d58b6931ead18fbca29ff7aa
Chris Churas <churas.camera@gmail.com>
2018-11-26 15:00:12 -0800

    to make code more testable moved more logic out of export into separate helper methods

--- src/main/java/org/ndexbio/common/importexport/ExporterExecutor.java
+++ src/main/java/org/ndexbio/common/importexport/ExporterExecutor.java
@@ -5,13 +5,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.StandardCopyOption;
-import java.sql.SQLException;
 import java.util.UUID;
 import java.util.logging.Logger;
 
 import org.apache.commons.io.IOUtils;
-import org.ndexbio.model.exceptions.NdexException;
-import org.ndexbio.model.exceptions.ObjectNotFoundException;
 import org.ndexbio.rest.Configuration;
 
 public class ExporterExecutor {
@@ -19,6 +16,10 @@ public class ExporterExecutor {
     private Logger _logger = Logger.getLogger(ExporterExecutor.class.getSimpleName());
 
 	public static final String STDERR_EXT = ".stderr";
+	
+	public static final String PERIOD = ".";
+	
+	private String _errorMessage = null;
 	
 	private ImporterExporterEntry impExp;
 	
@@ -34,51 +35,128 @@ public class ExporterExecutor {
 	 * @param input CX format network as an InputStream
 	 * @param taskId Id of task
 	 * @param userId Id of user
-	 * @return exit code of command line process which is 0 for success otherwise failure
-	 * @throws IOException
-	 * @throws InterruptedException
-	 * @throws SQLException
-	 * @throws ObjectNotFoundException
-	 * @throws NdexException
+	 * @return -100 if userId is null, -200 if exception is caught 
+	 *         otherwise exit code of command line process 0 for success
+	 *         and any other number for failure 
 	 */
-	public int export(InputStream input, UUID taskId, UUID userId) throws IOException, InterruptedException, SQLException, ObjectNotFoundException, NdexException {
+	public int export(InputStream input, UUID taskId, UUID userId) {
 		
-        String pathPrefix = this.createOutputDirectory(userId);
+		//clear error message
+		_errorMessage = null;
+        String pathPrefix = createOutputDirectory(userId);
+        if (pathPrefix == null) {
+        	return -100;
+        }
+
 		ProcessBuilder pb = new ProcessBuilder(impExp.getExporterCmd());
 		pb.directory(new File(impExp.getDirectoryName()));
-		Process p = pb.start();
-		try (OutputStream out = p.getOutputStream()) {
-			try (InputStream in = p.getInputStream() ) {
-				try (InputStream err = p.getErrorStream()) {
-					IOThreadHandler expHandler = new IOThreadHandler(in, pathPrefix+ "/"+ taskId + "." + impExp.getFileExtension());
-					expHandler.start();
-					IOThreadHandler errHandler = new IOThreadHandler(err, pathPrefix +
-							File.separator + taskId + ExporterExecutor.STDERR_EXT);
-					errHandler.start();
-					IOUtils.copy(input,out);
-					input.close();
-					out.close();
-					p.waitFor();
-				}
-			}			
+		try {
+			Process p = pb.start();
+			try (OutputStream out = p.getOutputStream()) {
+				try (InputStream in = p.getInputStream() ) {
+					try (InputStream err = p.getErrorStream()) {
+						IOThreadHandler expHandler = new IOThreadHandler(in, pathPrefix+ File.separator +
+								                                         taskId + ExporterExecutor.PERIOD +
+							                                             impExp.getFileExtension());
+						expHandler.start();
+						IOThreadHandler errHandler = new IOThreadHandler(err, pathPrefix +
+						 	                                             File.separator +
+						 	                                             taskId +
+						 	                                             ExporterExecutor.STDERR_EXT);
+						errHandler.start();
+						IOUtils.copy(input,out);
+						input.close();
+						out.close();
+						p.waitFor();
+					}
+				}		
+			}
+			return p.exitValue();
+		} catch(IOException ioex) {
+			this._errorMessage = "Caught IOException: " + ioex.getMessage();
+		} catch(InterruptedException ie) {
+			this._errorMessage = "Process was interrupted";
 		}
-		return p.exitValue();
+		return -200;
 	}
 		
 	/**
 	 * Creates directory for user that is used to write export results to
 	 * @param userID Id of user
-	 * @throws IOException if there is a problem creating the directory
+	 * @return String set to file system path prefix
 	 */
-	protected String createOutputDirectory(UUID userId) throws IOException {
-		String pathPrefix = Configuration.getInstance().getNdexRoot() + "/workspace/" + userId.toString();
-        File archiveDir = new File(pathPrefix);
-        if (!archiveDir.exists())
-        	_logger.fine("Creating directory: " + archiveDir.getCanonicalPath());
-        	archiveDir.mkdirs();
+	protected String createOutputDirectory(UUID userId) {
+		if (userId == null) {
+			_errorMessage = "User UUID is null";
+			return null;
+		}
+		String pathPrefix = getPathPrefix(userId);
+		if (pathPrefix == null) {
+			return null;
+		}
+		if (createDirectoryIfNeeded(pathPrefix) == false) {
+			return null;
+		}
+        
         return pathPrefix;
 	}
 	
+	/**
+	 * If path exists, verifies directory otherwise code attempts
+	 * to create it. 
+	 * @param path File system path as string
+	 * @return true if successful otherwise false and {@link #getErrorMessage()} 
+	 *         set with information about failure.
+	 */
+	protected boolean createDirectoryIfNeeded(String path) {
+		File archiveDir = new File(path);
+		
+		if (archiveDir.isDirectory() == true) {
+			return true;
+		}
+		if (archiveDir.exists()) {
+			_errorMessage = path + " exists, but is not a directory";
+			return false;
+		}
+        try {
+        	_logger.fine("Creating directory: " + archiveDir.getCanonicalPath());
+        } catch(IOException ioex){
+        	_logger.fine("Weird...Caught exception getting canonical path" +
+        	             ioex.getMessage());
+        }
+        boolean result = archiveDir.mkdirs();
+        _logger.fine("mkdirs() returned " + result);
+        if (result == false) {
+        	_errorMessage = "There was a problem creating the directory " + path;
+        }
+        return result;
+	}
+	
+	/**
+	 * Gets users path prefix
+	 * @return Path as string or null if there was error in which case 
+	 *         calling {@link #getErrorMessage()} may provide more insight into issue
+	 */
+	protected String getPathPrefix(UUID userId) {
+		try {
+			return Configuration.getInstance().getNdexRoot() +
+					             File.separator + "workspace" +
+					             File.separator + userId.toString();
+		} catch(NullPointerException npe) {
+			_errorMessage = "Caught NullPointerException trying to build users path prefix";
+		}
+		return null;
+	}
+	
+	/**
+	 * Returns last error message, if any from invocation
+	 * of public methods for objects of this class
+	 * @returns String containing error message or null if no error
+	 */
+	public String getErrorMessage() {
+		return this._errorMessage;
+	}
+
 	private static class IOThreadHandler extends Thread {
 		
 		private InputStream inputStream;
