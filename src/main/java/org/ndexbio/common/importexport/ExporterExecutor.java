d3fee20fe0a389e1444f6a6ca42a49f893a1b243
Chris Churas <churas.camera@gmail.com>
2018-11-26 17:48:21 -0800

    removed print statements

--- src/main/java/org/ndexbio/common/importexport/ExporterExecutor.java
+++ src/main/java/org/ndexbio/common/importexport/ExporterExecutor.java
@@ -68,8 +68,6 @@ public class ExporterExecutor {
 						input.close();
 						out.close();
 						p.waitFor();
-						System.out.println("YYYYYY" + expHandler.getErrorMessage());
-						System.out.println("XXXXXXX" + errHandler.getErrorMessage());
 					}
 				}		
 			}
