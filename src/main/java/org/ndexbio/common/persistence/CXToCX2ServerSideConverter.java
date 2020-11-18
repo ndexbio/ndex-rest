eda482298f8c56bc1e0138b96aeca1e710b7ab55
Jing Chen <jic002@ucsd.edu>
2020-11-18 13:42:31 -0700

    Minor refactor to keep the cx2 converter prefix in warings.

--- src/main/java/org/ndexbio/common/persistence/CXToCX2ServerSideConverter.java
+++ src/main/java/org/ndexbio/common/persistence/CXToCX2ServerSideConverter.java
@@ -26,7 +26,6 @@ import org.ndexbio.cx2.aspect.element.core.CxVisualProperty;
 import org.ndexbio.cx2.aspect.element.cytoscape.VisualEditorProperties;
 import org.ndexbio.cx2.converter.AspectAttributeStat;
 import org.ndexbio.cx2.converter.CX2VPHolder;
-import org.ndexbio.cx2.converter.CXToCX2LargeFileConverter;
 import org.ndexbio.cx2.converter.CXToCX2VisualPropertyConverter;
 import org.ndexbio.cx2.io.CX2AspectWriter;
 import org.ndexbio.cx2.io.CXWriter;
@@ -41,7 +40,6 @@ import org.ndexbio.cxio.aspects.datamodels.NodesElement;
 import org.ndexbio.cxio.core.AspectIterator;
 import org.ndexbio.cxio.core.writers.NiceCXCX2Writer;
 import org.ndexbio.cxio.metadata.MetaDataCollection;
-import org.ndexbio.cxio.metadata.MetaDataElement;
 import org.ndexbio.model.cx.NamespacesElement;
 import org.ndexbio.model.exceptions.NdexException;
 
@@ -69,7 +67,6 @@ public class CXToCX2ServerSideConverter {
 	
 	CXToCX2VisualPropertyConverter vpConverter;
 
-	public static final String messagePrefix = "CX2-CONVERTER: ";
 	
 	private static final int maximumNumberWarningMessages = 20;
 	
@@ -112,7 +109,7 @@ public class CXToCX2ServerSideConverter {
 		if (warnings.size() >= maximumNumberWarningMessages)
 			return;
 		
-		warnings.add(messagePrefix + warningStr);		
+		warnings.add(NiceCXCX2Writer.messagePrefix + warningStr);		
 	}
         
     /**
@@ -209,7 +206,7 @@ public class CXToCX2ServerSideConverter {
 				}
 			}		
 			
-			// add @context as network attribute if it is a seperate aspect
+			// add @context as network attribute if it is a separate aspect
 			if ( attrStats.hasNamespacesAspect()) {
 				ObjectMapper om = new ObjectMapper();
 				NamespacesElement namespaces = null;
@@ -498,53 +495,7 @@ public class CXToCX2ServerSideConverter {
 		
 		return edgeTable;
 	}
-	
-	
-	/* warning: this function can only be called after attrStats is initialized */
-/*	private List<CxMetadata> getCX2Metadata() {
-		List<CxMetadata> result = new ArrayList<>(metaDataCollection.size());
-				
-		MetaDataElement networkAttribute = metaDataCollection.getMetaDataElement(NetworkAttributesElement.ASPECT_NAME);
-		if ( networkAttribute != null ) {
-			result.add(new CxMetadata (CxNetworkAttribute.ASPECT_NAME, 1L));
-		}	
-
-		MetaDataElement vpM = metaDataCollection.getMetaDataElement( CyVisualPropertiesElement.ASPECT_NAME);
-		if ( vpM != null) {
-			result.add(new CxMetadata(CxVisualProperty.ASPECT_NAME, 1L));
-			
-			//addCx2 extra aspects
-			result.add(new CxMetadata(VisualEditorProperties.ASPECT_NAME, 1L));
-			
-			if (attrStats.getNodeBypassCount() > 0) {
-				result.add(new CxMetadata(CxNodeBypass.ASPECT_NAME,
-						attrStats.getNodeBypassCount()));
-			}
-			
-			if (attrStats.getEdgeBypassCount() > 0) {
-				result.add(new CxMetadata(CxEdgeBypass.ASPECT_NAME, attrStats.getEdgeBypassCount()));
-			}
-		}
 		
-		if (!attrDeclarations.getDeclarations().isEmpty()) {
-			result.add(new CxMetadata(CxAttributeDeclaration.ASPECT_NAME,1L));
-		}
-		
-		for ( MetaDataElement e: metaDataCollection) {
-			String aspectName = e.getName();
-			if ( !aspectName.equals(NetworkAttributesElement.ASPECT_NAME) && 
-					!aspectName.equals(CyVisualPropertiesElement.ASPECT_NAME) &&
-					!aspectName.equals(NodeAttributesElement.ASPECT_NAME) &&
-					!aspectName.equals(EdgeAttributesElement.ASPECT_NAME) &&
-					!aspectName.equals(CartesianLayoutElement.ASPECT_NAME) ) {
-				result.add(new CxMetadata(e.getName(), e.getElementCount().longValue()));
-			}
-	
-		}
-		return result;
-
-	} */
-	
 	
 	private AspectAttributeStat analyzeAttributes() throws NdexException, IOException {
 		
@@ -645,285 +596,5 @@ public class CXToCX2ServerSideConverter {
 
 	} 
    
-	/*
-	   private void addVisuaProperty(CyVisualPropertiesElement elmt,CX2VPHolder holder) throws NdexException, IOException {
-	    	
-		    CxVisualProperty style = holder.getStyle();
-		    String po = elmt.getProperties_of();
-	    	if ( po.equals("network")) {
-
-	    		style.getDefaultProps().setNetworkProperties(vpConverter.convertNetworkVPs(elmt.getProperties()));
-	    	} else if( po.equals("nodes:default")) {
-
-	       		// get dependencies
-	    		String nodeSizeLockedStr = elmt.getDependencies().get("nodeSizeLocked");
-	    	    		
-	    		boolean nodeSizeLocked = ( nodeSizeLockedStr != null && nodeSizeLockedStr.equals("true"));   	
-	    		
-	    		this.visualDependencies.getProperties().put("nodeSizeLocked", Boolean.valueOf(nodeSizeLockedStr) );
-	    		this.visualDependencies.getProperties().put("nodeCustomGraphicsSizeSync", 
-	    				Boolean.valueOf(elmt.getDependencies().get("nodeCustomGraphicsSizeSync")) );
-
-	    		Map<String,String> cx1Properties = elmt.getProperties();
-	    		if ( nodeSizeLocked) {
-	    			String size = cx1Properties.get("NODE_SIZE");
-	    			if ( size != null) {
-	    				//TODO: need to check if we need to give a warning about upgrading cyNDEx2.
-	    				cx1Properties.put("NODE_WIDTH", size);
-	    				cx1Properties.put("NODE_HEIGHT", size);
-	    			}
-	    		}
-	    		style.getDefaultProps().setNodeProperties(vpConverter.convertEdgeOrNodeVPs(cx1Properties));
-	    		
-	    		// add mapping
-	    		SortedMap<String,Mapping> nodeMappings = elmt.getMappings();
-	    		
-	    		if ( nodeSizeLocked ) {
-	       			Mapping sizeMapping = nodeMappings.remove("NODE_SIZE");
-	       			if ( sizeMapping != null ) {
-	       				nodeMappings.put("NODE_WIDTH", sizeMapping);
-	       				nodeMappings.put("NODE_HEIGHT", sizeMapping);
-	       			}
-	    		}
-	    		
-	    		if ( nodeMappings != null && !nodeMappings.isEmpty()) {
-	    			processMappingEntry(nodeMappings, style.getNodeMappings());
-	    		}
-	    		
-	    	} else if ( po.equals("edges:default")) {
-	      		//Map<String, Object> defaultVp = getDefaultVP (style);
-	    		      		
-	    		SortedMap<String,String> cx1Properties = elmt.getProperties();
-	    		
-	    		this.visualDependencies.getProperties().putAll(elmt.getDependencies());
-	    		
-	    		// add dependencies
-	    		String arrowColorMatchesEdgeStr = elmt.getDependencies().get("arrowColorMatchesEdge");
-
-	    		this.visualDependencies.getProperties().put("arrowColorMatchesEdge", Boolean.valueOf(arrowColorMatchesEdgeStr) );
-	    		
-	    		boolean arrowColorMatchesEdge = (arrowColorMatchesEdgeStr != null && arrowColorMatchesEdgeStr.equals("true"));
-
-	    		if ( arrowColorMatchesEdge ) {
-	    			String ep = cx1Properties.get("EDGE_UNSELECTED_PAINT");
-	    			cx1Properties.put("EDGE_SOURCE_ARROW_UNSELECTED_PAINT", ep);
-	    			cx1Properties.put("EDGE_STROKE_UNSELECTED_PAINT", ep);
-	    			cx1Properties.put("EDGE_TARGET_ARROW_UNSELECTED_PAINT", ep);
-	    		}
-
-	    		style.getDefaultProps().setEdgeProperties(vpConverter.convertEdgeOrNodeVPs(cx1Properties));
-	    		
-	    		// add mapping
-	    		SortedMap<String,Mapping> edgeMappings = elmt.getMappings();
-	    		
-	    		if ( arrowColorMatchesEdge) {
-	    			Mapping m = edgeMappings.remove("EDGE_UNSELECTED_PAINT");
-	    			if ( m !=null) {
-	    				edgeMappings.put("EDGE_SOURCE_ARROW_UNSELECTED_PAINT", m);
-	    				edgeMappings.put("EDGE_STROKE_UNSELECTED_PAINT", m);
-	    				edgeMappings.put("EDGE_TARGET_ARROW_UNSELECTED_PAINT", m);
-	    			}
-	    		}	
-	    		
-	    		if ( edgeMappings != null && !edgeMappings.isEmpty()) {
-	    			processMappingEntry(edgeMappings, style.getEdgeMappings());
-	    		}
-	    		
-	    	} else if ( po.equals("nodes")) {  //node bypasses
-	    		
-	    		SortedMap<String,String> vps = elmt.getProperties();
-	    		
-	    		Boolean nodeSizeLocked = (Boolean)this.visualDependencies.getProperties().get("nodeSizeLocked");
-	    		if( nodeSizeLocked.booleanValue()) {
-	    			String nsize = vps.remove("NODE_SIZE");
-	    			if ( nsize != null) {
-	    				vps.put("NODE_WIDTH", nsize);
-	    				vps.put("NODE_HEIGHT",nsize);
-	    			}
-	    		}
-	    		
-	    		CxNodeBypass nodebp = new CxNodeBypass(elmt.getApplies_to().longValue(), 
-	    				vpConverter.convertEdgeOrNodeVPs(elmt.getProperties()));
-	    		
-	    		if ( !nodebp.getVisualProperties().isEmpty())
-	    			holder.getNodeBypasses().add(nodebp);
-	    	} else if ( po.equals("edges")) {  // edge bypasses
-	    		Map<String,Object> v = vpConverter.convertEdgeOrNodeVPs(elmt.getProperties());
-	    		if ( !v.isEmpty()) {
-	    			CxEdgeBypass edgebp = new CxEdgeBypass();
-	    			edgebp.setId(elmt.getApplies_to().longValue());
-	    			edgebp.setVisualProperties(v);
-	    			holder.getEdgeBypasses().add(edgebp);
-	    		}
-	    	} else {
-	    		throw new NdexException ("'" + po + "' is not a supported Cytoscape visual property group. Please upgrade your cyNDEx-2 app to the latest version in Cytoscape and try again.");
-	    	}
-	    } */
-	   
-	    /**
-	     * Get the default style object from a cx2 style object
-	     * @param cx2Style
-	     * @return
-	     */
-	  /*  private static Map<String, Object> getDefaultVP(DefaultVisualProperties cx2Style) {
-	    	Map<String, Object> defaultVp = (Map<String, Object>)cx2Style.get("default");
-			if ( defaultVp == null) {
-				defaultVp = new HashMap<> ();
-				cx2Style.put("default", defaultVp);
-			}
-			return defaultVp;
-	    } */
-	    
-/*		private void processMappingEntry(SortedMap<String, Mapping> nodeMappings,
-				Map<String, VisualPropertyMapping> v2NodeMappings) throws NdexException, IOException {
-			for (Map.Entry<String, Mapping> entry : nodeMappings.entrySet()) {
-				String vpName = entry.getKey();
-				String newVPName = vpConverter.getNewEdgeOrNodeProperty(vpName);
-				if (newVPName == null)
-					continue;
-
-				VisualPropertyMapping mappingObj = new VisualPropertyMapping();
-				String mappingType = entry.getValue().getType();
-				try {
-					mappingObj.setType(VPMappingType.valueOf(mappingType));
-				} catch ( IllegalArgumentException e) {
-					throw new NdexException ("Invalid mapping type '" + mappingType + "' found on visual property '" +vpName +"'.");
-				}
-				MappingDefinition defObj = new MappingDefinition();
-				mappingObj.setMappingDef(defObj);
-				String defString = entry.getValue().getDefinition();
-				try {
-					if (mappingType.equals("PASSTHROUGH")) {
-						String mappingAttrName = ConverterUtilities.getPassThroughMappingAttribute(defString);
-						defObj.setAttributeName(mappingAttrName);
-					} else if (mappingType.equals("DISCRETE")) {
-						List<Map<String, Object>> m = new ArrayList<>();
-						try {
-							MappingValueStringParser sp = new MappingValueStringParser(defString);
-
-							String col = sp.get("COL");
-							String t = sp.get("T");
-							int counter = 0;
-							while (true) {
-								final String k = sp.get("K=" + counter);
-								if (k == null) {
-									break;
-								}
-								final String v = sp.get("V=" + counter);
-
-								if (v == null) {
-									throw new NdexException(
-											"error: discrete mapping string is corruptted for " + defString);
-								}
-
-								Map<String, Object> mapEntry = new HashMap<>(2);
-								ConverterUtilitiesResult cRes = ConverterUtilities.cvtStringValueToObj(t, k);
-								addWarning(cRes);
-								mapEntry.put("v", cRes.getResult());
-								mapEntry.put("vp", vpConverter.getNewEdgeOrNodePropertyValue(vpName, v));
-								m.add(mapEntry);
-								counter++;
-							}
-
-							defObj.setAttributeName(col);
-							defObj.setMapppingList(m);
-						} catch (IOException e) {
-							addWarning("Corrupted data found in DISCRETE mapping on " + vpName +
-									". Please upgrade your cyNDEx-2 and Cytoscape to the latest version and reload this network. Cause: " + e.getMessage() );
-							System.err.println(e.getMessage());
-							continue;
-						}
-
-					} else { // continuous mapping
-						List<Map<String, Object>> m = new ArrayList<>();
-						MappingValueStringParser sp = new MappingValueStringParser(defString);
-						String col = sp.get("COL");
-						String t = sp.get("T");
-
-						Object min = null;
-						Boolean includeMin = null;
-						// Object max = null;
-						Object minVP = null;
-						// Object maxVP = null;
-
-						int counter = 0;
-						Map<String, Object> currentMapping = new HashMap<>();
-
-						while (true) {
-							final String L = sp.get("L=" + counter);
-							if (L == null) {
-								break;
-							}
-							Object LO = vpConverter.getNewEdgeOrNodePropertyValue(vpName, L);
-
-							final String E = sp.get("E=" + counter);
-							if (E == null) {
-								break;
-							}
-							// Object EO = vpConverter.getNewEdgeOrNodePropertyValue(vpName,E);
-
-							final String G = sp.get("G=" + counter);
-							if (G == null) {
-								break;
-							}
-							Object GO = vpConverter.getNewEdgeOrNodePropertyValue(vpName, G);
-
-							final String OV = sp.get("OV=" + counter);
-							if (OV == null) {
-								throw new NdexException(
-										"error: continuous mapping string is corruptted for " + defString);
-							}
-							ConverterUtilitiesResult cRes = ConverterUtilities.cvtStringValueToObj("double", OV);
-							addWarning(cRes);
-							
-							Object OVO = cRes.getResult();
-
-							if (counter == 0) { // min side
-								currentMapping.put("includeMin", Boolean.FALSE);
-								currentMapping.put("includeMax", Boolean.valueOf(E.equals(L)));
-								currentMapping.put("maxVPValue", LO);
-								currentMapping.put("max", OVO);
-								m.add(currentMapping);
-
-							} else {
-								currentMapping.put("includeMin", includeMin);
-								currentMapping.put("includeMax", Boolean.valueOf(E.equals(L)));
-								currentMapping.put("minVPValue", minVP);
-								currentMapping.put("min", min);
-								currentMapping.put("maxVPValue", LO);
-								currentMapping.put("max", OVO);
-								m.add(currentMapping);
-							}
-
-							// store the max values as min for the next segment
-							includeMin = Boolean.valueOf(E.equals(G));
-
-							min = OVO;
-							minVP = GO;
-
-							currentMapping = new HashMap<>();
-							counter++;
-						}
-
-						// add the last entry
-						currentMapping.put("includeMin", includeMin);
-						currentMapping.put("includeMax", Boolean.FALSE);
-						currentMapping.put("minVPValue", minVP);
-						currentMapping.put("min", min);
-						m.add(currentMapping);
-
-						// add the list
-						defObj.setAttributeName(col);
-						defObj.setMapppingList(m);
-					}
-				} catch (NdexException e) {
-					throw new NdexException(
-							"Can't converter " + mappingType + " mapping on " + vpName + ". Cause: " + e.getMessage());
-				}
-				if ( mappingObj.getType() == VPMappingType.PASSTHROUGH || 
-						mappingObj.getMappingDef().getMapppingList().size()>0)
-				   v2NodeMappings.put(newVPName, mappingObj);
-			}
-		} */
 	   
 }
