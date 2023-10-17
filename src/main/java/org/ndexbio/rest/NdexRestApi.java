45b2bcfd443be610847a1a54ad66f2657a54a375
jingjingbic <jingjingbic@gmail.com>
2023-10-17 16:51:09 -0700

    Adding v3 batch services to API.

--- src/main/java/org/ndexbio/rest/NdexRestApi.java
+++ src/main/java/org/ndexbio/rest/NdexRestApi.java
@@ -63,6 +63,7 @@ import org.ndexbio.rest.services.TaskService;
 import org.ndexbio.rest.services.TaskServiceV2;
 import org.ndexbio.rest.services.UserService;
 import org.ndexbio.rest.services.UserServiceV2;
+import org.ndexbio.rest.services.v3.BatchService;
 import org.ndexbio.rest.services.v3.CyWebWorkspaceServices;
 import org.ndexbio.rest.services.v3.NetworkServiceV3;
 import org.ndexbio.rest.services.v3.SearchServiceV3;
@@ -94,6 +95,7 @@ public class NdexRestApi extends Application
         _resources.add(SearchServiceV3.class);
         _resources.add(CyWebWorkspaceServices.class);
         _resources.add(UserServicesV3.class);
+        _resources.add(BatchService.class);
         
         
         
