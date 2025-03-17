04917b3ba99dee209f8c41b56bb8f3e9e4af12c5
dzoanka <joanna.lenkiewicz@gmail.com>
2025-03-17 15:17:35 -0700

    Initial revision

---
+++ src/main/java/org/ndexbio/common/models/dao/postgresql/ShortcutDAO.java
@@ -0,0 +1,155 @@
+package org.ndexbio.common.models.dao.postgresql;
+
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+import org.ndexbio.model.exceptions.NdexException;
+import org.ndexbio.model.exceptions.ObjectNotFoundException;
+import org.ndexbio.model.exceptions.UnauthorizedOperationException;
+import org.ndexbio.model.object.NdexObjectUpdateStatus;
+import org.ndexbio.model.object.Shortcut;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+public class ShortcutDAO extends NdexDBDAO {
+	
+	private static Logger logger = Logger.getLogger(ShortcutDAO.class.getName());
+
+	public ShortcutDAO() throws SQLException {
+		super();
+	}
+	
+	public NdexObjectUpdateStatus createShortcut(final UUID shortcutUUID, final UUID ownerId, final UUID parentUUID, final String name, final UUID targetUUID) throws SQLException {
+		Timestamp t = new Timestamp(System.currentTimeMillis());
+		
+		String sqlStr = "insert into shortcut (\"UUID\", creation_time, modification_time, is_deleted, name, target_type, target, visibility, owneruuid, parent) values"
+				+ "(?, ?, ?, false, ?, ?, ?, 'PRIVATE',?, ?) ";
+		try (PreparedStatement pst = db.prepareStatement(sqlStr)) {
+			pst.setObject(1, shortcutUUID);
+			pst.setTimestamp(2, t);
+			pst.setTimestamp(3, t);
+			pst.setString(4, name);
+			pst.setString(5, "folder"); //TODO: add a method to check type of target
+			pst.setObject(6, targetUUID);
+			pst.setObject(7, ownerId);
+			pst.setObject(8, parentUUID);
+			
+			pst.executeUpdate();
+		}
+		NdexObjectUpdateStatus result = new NdexObjectUpdateStatus();
+		result.setModificationTime(t);
+		result.setUuid(shortcutUUID);
+		return result;
+	}
+	
+	protected static String createIsReadableConditionStr(UUID userId) {
+	    if (userId == null) {
+	        // Anonymous user => only PUBLIC is allowed
+	        return "s.visibility='PUBLIC'";
+	    }
+	    // Non-anonymous => public or same owner
+	    return "( s.visibility='PUBLIC' "
+	         + "  OR s.owneruuid = '" + userId + "'::uuid "
+	         + ")";
+	}
+	
+	public boolean isReadable(UUID shortcutID, UUID userId) throws SQLException, ObjectNotFoundException {
+		String sqlStr = "select (" + createIsReadableConditionStr(userId) + ") from shortcut s where s.\"UUID\" = ? and s.is_deleted=false ";		
+			
+		try (PreparedStatement pst = db.prepareStatement(sqlStr)) {
+			pst.setObject(1, shortcutID);
+
+			try ( ResultSet rs = pst.executeQuery()) {
+				if ( rs.next()) 
+					return rs.getBoolean(1);
+				 
+				throw new ObjectNotFoundException("Shortcut", shortcutID);
+			}
+		}
+	}
+	
+	public boolean accessKeyIsValid(UUID shortcutId, String accessKey) throws SQLException {
+		if ( accessKey ==null || accessKey.length() == 0)
+			return false;
+		
+		String sqlStr = "select 1 from shortcut where (\"UUID\"=? and access_key_is_on and access_key = ?)" ;
+		try (PreparedStatement p = db.prepareStatement(sqlStr)) {
+			p.setObject(1, shortcutId);
+			p.setString(2, accessKey);
+			try ( ResultSet rs = p.executeQuery()) {
+				 if (rs.next())
+					 return true;
+			}		
+		}
+		return true;
+
+	}
+	
+	public Shortcut getShortcut(UUID shortcutId, UUID userId, String accessKey) throws SQLException, ObjectNotFoundException, UnauthorizedOperationException, JsonParseException, JsonMappingException, IOException {
+		
+		Shortcut result = new Shortcut();
+		String sqlStr = "select creation_time, modification_time, name, target, parent, is_deleted from shortcut where \"UUID\"=?";
+		
+		try (PreparedStatement p = db.prepareStatement(sqlStr)) {
+			p.setObject(1, shortcutId);
+			try ( ResultSet rs = p.executeQuery()) {
+				if ( rs.next()) {
+					result.setCreationTime(rs.getTimestamp(1));
+					result.setModificationTime(rs.getTimestamp(2));
+					result.setExternalId(shortcutId);
+					result.setName(rs.getString(3));
+					result.setTarget((UUID)(rs.getObject(4)));
+					result.setParent((UUID)(rs.getObject(5)));
+					result.setIsDeleted(rs.getBoolean(6));
+				} else
+					throw new ObjectNotFoundException("Shortcut" + shortcutId + " not found in db.");
+			}
+		}
+		
+		return result;
+	}
+	
+	public boolean isShortcutOwner(UUID shortcutId, UUID ownerId) throws SQLException {
+		String sqlStr = "select 1 from shortcut where \"UUID\" = ? and owneruuid = ? and is_deleted=false";
+		try (PreparedStatement p = db.prepareStatement(sqlStr)) {
+			p.setObject(1, shortcutId);
+			p.setObject(2, ownerId);
+			try ( ResultSet rs = p.executeQuery()) {
+				return rs.next(); 
+			}
+		}
+	}
+	
+	public void deleteShortcut(UUID shortcutId) throws SQLException {
+		Timestamp t = new Timestamp(System.currentTimeMillis());
+		
+		String sqlStr = "update shortcut set modification_time = ?, is_deleted = true  where \"UUID\"=?";
+				
+		try (PreparedStatement pst = db.prepareStatement(sqlStr)) {
+			pst.setTimestamp(1, t);
+			pst.setObject(2, shortcutId);
+			pst.executeUpdate();
+		}
+		
+	}
+	
+	public void updateShortcut(UUID shortcutId, String name, UUID ownerId) throws SQLException, JsonProcessingException, NdexException {
+		Timestamp t = new Timestamp(System.currentTimeMillis());
+		String sqlStr = "update shortcut set modification_time = ?, name = ? where \"UUID\"=? and is_deleted=false";
+		
+		try (PreparedStatement pst = db.prepareStatement(sqlStr)) {
+			pst.setTimestamp(1, t);
+			pst.setString(2, name);
+			pst.setObject(3, shortcutId);
+			pst.executeUpdate();
+		}
+	}
+
+}
---
+++ src/main/java/org/ndexbio/rest/services/v3/files/ShortcutServiceV3.java
@@ -0,0 +1,157 @@
+package org.ndexbio.rest.services.v3.files;
+
+import java.net.URI;
+import java.sql.SQLException;
+import java.util.UUID;
+
+import org.ndexbio.common.models.dao.postgresql.ShortcutDAO;
+import org.ndexbio.common.util.NdexUUIDFactory;
+import org.ndexbio.model.exceptions.DuplicateObjectException;
+import org.ndexbio.model.exceptions.NdexException;
+import org.ndexbio.model.exceptions.UnauthorizedOperationException;
+import org.ndexbio.model.object.NdexObjectUpdateStatus;
+import org.ndexbio.model.object.Shortcut;
+import org.ndexbio.model.object.ShortcutRequest;
+import org.ndexbio.rest.Configuration;
+import org.ndexbio.rest.filters.BasicAuthenticationFilter;
+import org.ndexbio.rest.services.NdexService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.annotation.security.PermitAll;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/v3/files/shortcuts")
+public class ShortcutServiceV3 extends NdexService {
+
+	protected static Logger accLogger = LoggerFactory.getLogger(BasicAuthenticationFilter.accessLoggerName);
+	
+	public ShortcutServiceV3(@Context HttpServletRequest httpRequest) {
+		super(httpRequest);
+	}
+	
+	
+	@POST
+	@Path("/")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Response createShortcut(final ShortcutRequest request) throws Exception {
+		if (request == null) {
+			throw new Exception("No shortcut request provided.");
+		}
+        if (request.getName() == null || request.getName().isEmpty()) {
+            throw new Exception("Shortcut name cannot be empty.");
+        }
+        if (request.getTarget() == null) {
+            throw new Exception("Shortcut 'target' cannot be null.");
+        }
+		
+		UUID shortcutUUID = NdexUUIDFactory.INSTANCE.createNewNDExUUID();
+		
+		NdexObjectUpdateStatus status;
+		try (ShortcutDAO dao = new ShortcutDAO()) {
+			status = dao.createShortcut(shortcutUUID, getLoggedInUser().getExternalId(), request.getParent(), request.getName(), request.getTarget());
+			dao.commit();
+		}
+		
+		String urlStr = Configuration.getInstance().getHostURI() +"/v3/files/shortcuts/"+ shortcutUUID.toString();
+			
+		URI l = new URI (urlStr);
+		ObjectMapper om = new ObjectMapper();
+		
+		return Response.created(l).header("Access-Control-Expose-Headers", "Location")
+				.entity(om.writeValueAsString(status)).build();
+
+		}
+	
+	@PermitAll
+	@GET
+	@Path("/{shortcutid}")
+	@Operation(summary = "Get Shortcut", description = "XXX")
+
+	public Response getShortcut(	@PathParam("shortcutid") final String shortcutId,
+			@QueryParam("accesskey") String accessKey,
+			@QueryParam("id_token") String id_token,
+			@QueryParam("auth_token") String auth_token)
+			throws Exception {
+		
+    	Shortcut shortcut = null;
+    	
+    	try (ShortcutDAO dao = new ShortcutDAO()) {
+    		UUID shortcutUUID = UUID.fromString(shortcutId);
+
+     		UUID userId = getLoggedInUserId();
+    		if ( userId == null ) {
+    			if ( auth_token != null) {
+    				userId = getUserIdFromBasicAuthString(auth_token);
+    			} else if ( id_token !=null) {
+    				if ( getOAuthAuthenticator() == null)
+    					throw new UnauthorizedOperationException("Google OAuth is not enabled on this server.");
+    				userId = getOAuthAuthenticator().getUserUUIDByIdToken(id_token);
+    			}
+    		}
+    		if ( ! dao.isReadable(shortcutUUID, userId) && (!dao.accessKeyIsValid(shortcutUUID, accessKey))) 
+                throw new UnauthorizedOperationException("User doesn't have read access to this network.");
+    		shortcut = dao.getShortcut(shortcutUUID, userId, accessKey);
+    	}
+
+    	return 	Response.ok().type(MediaType.APPLICATION_JSON_TYPE).entity(shortcut).build();
+		
+	}
+	
+	@DELETE
+	@Path("/{shortcutid}")
+	@Operation(summary = "Delete a Shortcut", description = "Delete a shortcut.")
+	@Produces("application/json")
+	public void deleteShortcut(@PathParam("shortcutid") final String shortcutIdStr)
+			throws NdexException, SQLException {
+		
+		UUID shortcutId = UUID.fromString(shortcutIdStr);
+		try (ShortcutDAO dao = new ShortcutDAO()){
+			
+			if (!dao.isShortcutOwner(shortcutId, getLoggedInUserId()))
+				throw new UnauthorizedOperationException("Signed in user is not the owner of this shortcut.");
+				
+			dao.deleteShortcut(shortcutId);
+			dao.commit();
+		} 
+	}
+	
+	@PUT
+	@Path("/{shortcutid}")
+	@Consumes("application/json")
+	@Produces("application/json")
+	@Operation(summary = "Rename a Shortcut", description = "Rename a shortcut.")
+	public void updateShortcut(@QueryParam("name") String nameStr,
+			@PathParam("shortcutid") final String shortcutIdStr)
+			throws  DuplicateObjectException,
+			NdexException,  SQLException, JsonProcessingException {
+
+		UUID shortcutId = UUID.fromString(shortcutIdStr);
+		try (ShortcutDAO dao = new ShortcutDAO()){
+			if ( !dao.isShortcutOwner(shortcutId, getLoggedInUserId()))
+				throw new UnauthorizedOperationException("Signed in user is not the owner of this shortcut.");
+			
+			dao.updateShortcut(shortcutId, nameStr, getLoggedInUserId());
+			dao.commit();	
+			return;
+		} 
+	}
+
+}
