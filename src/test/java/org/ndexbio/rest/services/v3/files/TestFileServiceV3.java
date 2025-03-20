f3ce170ddb5446bd60d514d672078e8a90047948
Chris Churas <churas.camera@gmail.com>
2025-03-19 20:20:13 -0700

    registered exception mapper in mock dispatcher and use the objectmapper to get NDExError object

--- src/test/java/org/ndexbio/rest/services/v3/files/TestFileServiceV3.java
+++ src/test/java/org/ndexbio/rest/services/v3/files/TestFileServiceV3.java
@@ -3,8 +3,7 @@ package org.ndexbio.rest.services.v3.files;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
-import java.nio.charset.StandardCharsets;
-import org.apache.zookeeper.proto.ErrorResponse;
+import jakarta.ws.rs.core.Response.Status;
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
@@ -12,6 +11,8 @@ import org.jboss.resteasy.mock.*;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.jboss.resteasy.spi.Dispatcher;
+import org.ndexbio.model.errorcodes.NDExError;
+import org.ndexbio.rest.exceptions.mappers.UnauthorizedOperationExceptionMapper;
 
 /**
  *
@@ -32,23 +33,39 @@ public class TestFileServiceV3 {
             expect(mockHttpServletRequest.getAttribute("User")).andReturn(null);
             replay(mockHttpServletRequest);
 			
+			// create a mock dispatcher
             Dispatcher dispatcher = MockDispatcherFactory.createDispatcher();
+			
+			//register the class of the endpoint you want to test
             dispatcher.getRegistry().addSingletonResource(new FileServiceV3(mockHttpServletRequest));
+			
+			//if test causes an exception to be thrown be sure to 
+			//register the mapper for that exception
+			dispatcher.getProviderFactory().registerProvider(UnauthorizedOperationExceptionMapper.class);
 
+			//Create a mock request
             MockHttpRequest request = MockHttpRequest.get("/v3/files/count");
 
+			// Create a mock response
             MockHttpResponse response = new MockHttpResponse();
             
-      
-            dispatcher.invoke(request, response);
-            assertEquals(500, response.getStatus());
-            //ObjectMapper mapper = new ObjectMapper();
-            //ErrorResponse er = mapper.readValue(response.getOutput(),
-             //       ErrorResponse.class);
-			String errStr = new String(response.getOutput(), StandardCharsets.UTF_8);
-            assertEquals("You must be signed in to see your file counts.", errStr);
-			//assertEquals("hi", response.getOutput().toString());
-            //assertEquals(ErrorCode.NDEx_Unauthorized_Operation_Exception, er.getErr());
+
+			// invoke the endpoint
+			dispatcher.invoke(request, response);
+			
+			// check the status of response
+            assertEquals(Status.UNAUTHORIZED.getStatusCode(), response.getStatus());
+			
+			// in this case we expected an exception so we are 
+			// mapping it back to NDExError object which is what
+			// the exceptionmapper generates
+            ObjectMapper mapper = new ObjectMapper();
+            NDExError er = mapper.readValue(response.getOutput(),
+                    NDExError.class);
+			
+			//Here we are verifying the correct message was set in the exception
+            assertEquals("You must be signed in to see your file counts.", er.getMessage());
+			
         } finally {
             //_folder.delete();
         }
