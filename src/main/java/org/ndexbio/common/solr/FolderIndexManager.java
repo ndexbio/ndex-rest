34d66da0edc12cb2ab2971c5e6d793f959894f7b
enoorzay <enoorzay@gmail.com>
2026-02-04 14:28:11 -0800

    add search fxnality for folders and index parent uuid

--- src/main/java/org/ndexbio/common/solr/FolderIndexManager.java
+++ src/main/java/org/ndexbio/common/solr/FolderIndexManager.java
@@ -1,9 +1,19 @@
 package org.ndexbio.common.solr;
 
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrRequest;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.BaseHttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrInputDocument;
+import org.ndexbio.model.exceptions.NdexException;
 import org.ndexbio.model.object.FileType;
 import org.ndexbio.model.object.NdexFolder;
+import org.ndexbio.model.object.Permissions;
 import org.ndexbio.model.object.network.VisibilityType;
+
+import java.io.IOException;
 
 public class FolderIndexManager extends NFSIndexManager<NdexFolder> {
 
@@ -24,7 +34,10 @@ public class FolderIndexManager extends NFSIndexManager<NdexFolder> {
         if (folder.getDescription() != null && folder.getDescription().length()>1){
             doc.addField(DESC, folder.getDescription());
         }
-        // @TODO do we want to index parent uuid?
+        if (folder.getParent() != null) {
+            doc.addField(PARENT_UUID, folder.getParent().toString());
+        }
+
         if (folder.getOwner() != null && !folder.getOwner().isBlank()) {
             doc.addField(USER_ADMIN, folder.getOwner());
         }
@@ -41,4 +54,42 @@ public class FolderIndexManager extends NFSIndexManager<NdexFolder> {
         return doc;
     }
 
+    @Override
+    protected String getQueryFields() {
+        // Folders are simpler - mainly name and description
+        return "uuid^20 name^10 description^5 owner^2";
+    }
+
+    /**
+     * Search for folders in a specific parent folder
+     */
+    public SolrDocumentList searchInFolder(
+            String searchTerms,
+            String userAccount,
+            int limit,
+            int offset,
+            String parentFolderId,
+            Permissions permission) throws IOException, SolrServerException, NdexException {
+
+        SolrQuery solrQuery = new SolrQuery();
+
+        String permissionFilter = buildPermissionFilter(userAccount, permission);
+        String typeFilter = " AND (" + ENTITY_TYPE + ":FOLDER)";
+        String parentFilter = "";
+
+        if (parentFolderId != null) {
+            parentFilter = " AND (" + PARENT_UUID + ":\"" + parentFolderId + "\")";
+        }
+
+        String resultFilter = permissionFilter + typeFilter + parentFilter;
+
+        configureQuery(solrQuery, searchTerms, resultFilter, limit, offset);
+
+        try {
+            QueryResponse rsp = client.query(solrQuery, SolrRequest.METHOD.POST);
+            return rsp.getResults();
+        } catch (BaseHttpSolrClient.RemoteSolrException e) {
+            throw convertException(e, coreName);
+        }
+    }
 }
