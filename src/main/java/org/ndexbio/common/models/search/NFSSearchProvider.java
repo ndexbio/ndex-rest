eba67901ca18ff87f746b3ebf15b636193073927
enoorzay <enoorzay@gmail.com>
2026-03-10 13:25:26 -0700

    Add DB lookups to search results to fetch more data in file item summary result

--- src/main/java/org/ndexbio/common/models/search/NFSSearchProvider.java
+++ src/main/java/org/ndexbio/common/models/search/NFSSearchProvider.java
@@ -3,6 +3,9 @@ package org.ndexbio.common.models.search;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrInputDocument;
+import org.ndexbio.common.models.dao.DAOFactory;
+import org.ndexbio.common.models.dao.FolderDAO;
+import org.ndexbio.common.models.dao.NetworkDAO;
 import org.ndexbio.common.models.dao.ShortcutDAO;
 import org.ndexbio.common.solr.NFSIndexManager;
 import org.ndexbio.common.solr.SolrClientWrapper;
@@ -10,6 +13,7 @@ import org.ndexbio.model.exceptions.NdexException;
 import org.ndexbio.model.exceptions.ObjectNotFoundException;
 import org.ndexbio.model.exceptions.UnauthorizedOperationException;
 import org.ndexbio.model.object.*;
+import org.ndexbio.model.object.network.NetworkSummary;
 import org.ndexbio.model.object.network.VisibilityType;
 import org.ndexbio.rest.Configuration;
 import org.slf4j.Logger;
@@ -39,38 +43,152 @@ public class NFSSearchProvider implements SearchProvider {
         String userAccessorId = accesser != null ? accesser.getUserName() : null;
         // accountName is an optional owner filter
         String ownedBy = query.getAccountName();
-
+        List<FileItemSummary> allSummaries;
         SolrDocumentList documents;
-        if (query.getType() != null) {
-            documents = delegate.searchByType(query.getSearchString(), userAccessorId,
-                    visibilityType,
-                    blockSize, skipBlocks, ownedBy,
-                    query.getPermission(), query.getType().toString());
-        } else {
-            documents = delegate.search(query.getSearchString(), userAccessorId,
-                    visibilityType, blockSize, skipBlocks, ownedBy,
-                    query.getPermission());
+        try {
+            if (query.getType() != null) {
+                documents = delegate.searchByType(query.getSearchString(), userAccessorId,
+                        visibilityType,
+                        blockSize, skipBlocks, ownedBy,
+                        query.getPermission(), query.getType().toString());
+                allSummaries = getDocumentSummariesByEntityType(query.getType(), getUUIDsFromDocuments(documents));
+
+            } else {
+                documents = delegate.search(query.getSearchString(), userAccessorId,
+                        visibilityType, blockSize, skipBlocks, ownedBy,
+                        query.getPermission());
+                List<UUID> sortedUUIDs = new ArrayList<>();
+                Map<FileType, List<UUID>> documentsByType = new HashMap<>();
+
+                for (SolrDocument document : documents) {
+                    String entityTypeStr = (String) document.get(NFSIndexManager.ENTITY_TYPE);
+                    FileType entityType = FileType.valueOf(entityTypeStr);
+                    UUID id = getUUIDFromDocument(document);
+                    sortedUUIDs.add(id);
+                    if (!documentsByType.containsKey(entityType)) {
+                        documentsByType.put(entityType, new ArrayList<>());
+                    }
+                    documentsByType.get(entityType).add(id);
+                }
+                Map<UUID, FileItemSummary> summariesById = new HashMap<>();
+                for (Map.Entry<FileType, List<UUID>> entry : documentsByType.entrySet()) {
+                    getDocumentSummariesByEntityType(entry.getKey(), entry.getValue())
+                            .forEach(summary ->
+                                    summariesById.put(summary.getUuid(), summary));
+                }
+                allSummaries = sortedUUIDs.stream()
+                        .map(summariesById::get)
+                        .toList();
+            }
+
+        } catch (Exception e) {
+            throw new NdexException("Failed to fetch data about search results.", e);
         }
-        List<FileItemSummary> fileItemSummaryList = documents
-                .stream()
-                .map(this::mapSolrDocumentToSummary)
-                .collect(Collectors.toList());
+        if (allSummaries == null) allSummaries = new ArrayList<>();
 
-        List<FileItemSummary> shortcuts = fileItemSummaryList.stream()
-                .filter(x->x.getType().equals(FileType.SHORTCUT))
-                .toList();
-
-        if (!shortcuts.isEmpty() && accesser!=null) {
-            try (ShortcutDAO shortcutDAO = Configuration.getInstance().getDAOFactory().getShortcutDAO()) {
-                shortcuts.forEach(shortcut -> addTargetTypeToShortcutSummaryItem(shortcut, shortcutDAO, accesser));
-            } catch (Exception ignored) {}
-        }
-
-
-        return new FileSearchResult(fileItemSummaryList.size(), (long) skipBlocks * blockSize, fileItemSummaryList);
+        return new FileSearchResult(allSummaries.size(), (long) skipBlocks * blockSize, allSummaries);
     }
 
+    private List<FileItemSummary> getDocumentSummariesByEntityType(FileType fileType, List<UUID> ids) throws Exception {
+        DAOFactory daoFactory = Configuration.getInstance().getDAOFactory();
+        List<FileItemSummary> result;
+        if (fileType.equals(FileType.SHORTCUT)){
+            try (ShortcutDAO shortcutDAO = daoFactory.getShortcutDAO()){
+                result = shortcutDAO.getShortcutsByIds(ids).stream()
+                        .map(this::mapShortcutToSummary)
+                        .toList();
+            }
+        } else if (fileType.equals(FileType.FOLDER)) {
+            try (FolderDAO folderDAO = daoFactory.getFolderDAO()){
+                result =  folderDAO.getFoldersByIds(ids).stream()
+                        .map(this::mapFolderToSummary)
+                        .toList();
+            }
+        }
+        else if (fileType.equals(FileType.NETWORK)){
+            try (NetworkDAO networkDAO = daoFactory.getNetworkDAO()){
+                result =  networkDAO.getNetworkSummariesByIds(ids).stream()
+                        .map(this::mapNetworkToSummary)
+                        .toList();
+            }
+        }
+        else {
+            throw new NdexException("Invalid entity type found: " + fileType);
+        }
+        return result;
+    }
 
+    private FileItemSummary mapShortcutToSummary(NdexShortcut ndexShortcut) {
+        FileItemSummary fis = new FileItemSummary();
+        fis.setUuid(ndexShortcut.getExternalId());
+        fis.setType(FileType.SHORTCUT);
+        fis.setName(ndexShortcut.getName());
+        fis.setModificationTime(ndexShortcut.getModificationTime());
+
+        Map<String, Object> attr = new HashMap<>();
+        attr.put("parent", ndexShortcut.getParent());
+        attr.put("target", ndexShortcut.getTarget());
+        attr.put("target_type", ndexShortcut.getTargetType() != null ? ndexShortcut.getTargetType().toString() : null);
+        attr.put("creationTime", ndexShortcut.getCreationTime());
+        fis.setAttributes(attr);
+
+        fis.setOwner(ndexShortcut.getOwner());
+
+        return fis;
+    }
+    private FileItemSummary mapFolderToSummary(NdexFolder ndexFolder) {
+        FileItemSummary fis = new FileItemSummary();
+        fis.setUuid(ndexFolder.getExternalId());
+        fis.setType(FileType.FOLDER);
+        fis.setName(ndexFolder.getName());
+        fis.setModificationTime(ndexFolder.getModificationTime());
+
+        Map<String, Object> attr = new HashMap<>();
+        attr.put("description", ndexFolder.getDescription());
+        attr.put("parent", ndexFolder.getParent());
+        attr.put("creationTime", ndexFolder.getCreationTime());
+        fis.setAttributes(attr);
+
+        fis.setOwner(ndexFolder.getOwner());
+
+        return fis;
+    }
+    private FileItemSummary mapNetworkToSummary(NetworkSummary networkSummary) {
+        FileItemSummary fis = new FileItemSummary();
+        fis.setUuid(networkSummary.getExternalId());
+        fis.setType(FileType.NETWORK);
+        fis.setName(networkSummary.getName());
+        fis.setModificationTime(networkSummary.getModificationTime());
+        fis.setOwner(networkSummary.getOwner());
+        fis.setOwnerId(networkSummary.getOwnerUUID());
+        fis.setVisibility(networkSummary.getVisibility() != null ? networkSummary.getVisibility().toString() : null);
+        fis.setEdges(networkSummary.getEdgeCount());
+        fis.setIsReadOnly(networkSummary.getIsReadOnly());
+        fis.setErrorMessage(networkSummary.getErrorMessage());
+        fis.setWarnings(networkSummary.getWarnings());
+        fis.setIsCompleted(networkSummary.isCompleted());
+        fis.setIsValid(networkSummary.getIsValid());
+        fis.setDoi(networkSummary.getDoi());
+
+        Map<String, Object> attr = new HashMap<>();
+        attr.put("description", networkSummary.getDescription());
+        attr.put("version", networkSummary.getVersion());
+        attr.put("nodeCount", networkSummary.getNodeCount());
+        attr.put("creationTime", networkSummary.getCreationTime());
+        attr.put("isShowcase", networkSummary.getIsShowcase());
+        attr.put("isCertified", networkSummary.getIsCertified());
+        attr.put("indexLevel", networkSummary.getIndexLevel() != null ? networkSummary.getIndexLevel().toString() : null);
+        attr.put("hasLayout", networkSummary.getHasLayout());
+        attr.put("hasSample", networkSummary.getHasSample());
+        attr.put("cxFormat", networkSummary.getCxFormat());
+        attr.put("cxFileSize", networkSummary.getCxFileSize());
+        attr.put("cx2FileSize", networkSummary.getCx2FileSize());
+        attr.put("folderId", networkSummary.getFolderId());
+        attr.put("subnetworkIds", networkSummary.getSubnetworkIds());
+        fis.setAttributes(attr);
+
+        return fis;
+    }
     private FileItemSummary mapSolrDocumentToSummary(SolrDocument solrDocument){
         String uuid = (String)solrDocument.get(NFSIndexManager.UUID);
 
@@ -81,6 +199,14 @@ public class NFSSearchProvider implements SearchProvider {
         String name = (String)solrDocument.getOrDefault(NFSIndexManager.NAME, "unknown");
 
         return new FileItemSummary(UUID.fromString(uuid), fileType, name);
+    }
+    private List<UUID> getUUIDsFromDocuments(SolrDocumentList solrDocuments){
+        return solrDocuments.stream()
+                .map(this::getUUIDFromDocument)
+                .toList();
+    }
+    private UUID getUUIDFromDocument(SolrDocument solrDocument){
+        return (UUID)solrDocument.get(NFSIndexManager.UUID);
     }
 
     private void addTargetTypeToShortcutSummaryItem(FileItemSummary fileItemSummary, ShortcutDAO shortcutDAO,
